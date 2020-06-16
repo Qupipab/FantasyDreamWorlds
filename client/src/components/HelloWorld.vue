da8ee5c16d3558ef5cbc8f2eea83d743e3869609
Qupipab <QupipabJob@gmail.com>
2020-06-17 00:59:41 +0300

    update project settings

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -1,4 +1,5 @@
 <template>
+  <!-- eslint-disable -->
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
@@ -40,7 +41,7 @@ export default {
   props: {
     msg: String
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
