4e731a87e4707175ccf51fb8708ca6dd5fae67e3
Qupipab <QupipabJob@gmail.com>
2020-06-17 01:20:18 +0300

    update header and footer folders name, add path to header and footer #1

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,1 +1,3 @@
 export { default as HelloWorld } from '@components/HelloWorld.vue';
+export { default as Header } from '@components/fdw-header/fdw-header.component.vue';
+export { default as Footer } from '@components/fdw-footer/fdw-footer.component.vue';
