8ce3b29a1e65ec486b54c5a5143c21b5c77753e1
Qupipab <QupipabJob@gmail.com>
2020-06-17 02:02:22 +0300

    add test vuetify component and add styles path #1

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -2,16 +2,19 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-btn>test btn</v-btn>
   </div>
 </template>
 
 <script>
+import { VBtn } from 'vuetify/lib';
 import { HelloWorld } from '@components';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
+    VBtn
   }
 };
 </script>
