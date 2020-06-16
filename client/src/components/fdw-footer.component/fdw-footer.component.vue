f7f37c410ceed9f5bb2b518b0718d67cf579fc43
Qupipab <QupipabJob@gmail.com>
2020-06-17 01:12:55 +0300

    init footer and header component #1

---
+++ client/src/components/fdw-footer.component/fdw-footer.component.vue
@@ -0,0 +1,5 @@
+<template src="./fdw-footer.component.html"></template>
+
+<script src="./fdw-footer.component.js"></script>
+
+<style scoped src="./fdw-footer.component.css"></style>
---
+++ client/src/components/fdw-header.component/fdw-header.component.vue
@@ -0,0 +1,5 @@
+<template src="./fdw-header.component.html"></template>
+
+<script src="./fdw-header.component.js"></script>
+
+<style scoped src="./fdw-header.component.css"></style>
