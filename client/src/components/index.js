69265aaefee1b9327f12f89e416b110d2a8b75ec
Qupipab <QupipabJob@gmail.com>
2020-06-25 22:53:46 +0300

    #13 fix issue

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,3 +1,3 @@
 export { default as HelloWorld } from '@components/HelloWorld.vue';
 export { default as FdwHeader } from '@components/fdw-header/fdw-header.component.vue';
-export { default as Footer } from '@components/fdw-footer/fdw-footer.component.vue';
+export { default as FdwFooter } from '@components/fdw-footer/fdw-footer.component.vue';
