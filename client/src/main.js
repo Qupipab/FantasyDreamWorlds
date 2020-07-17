9dd52b599bcf40ec7f23cdf2a0815b71eb762f4b
Yaronberg <yaronmen123@gmail.com>
2020-07-17 22:59:35 +0300

    #3 add api global function

--- client/src/main.js
+++ client/src/main.js
@@ -1,9 +1,9 @@
 import './registerServiceWorker';
 
-import { Default } from '@layout';
-
-import Vue from 'vue';
 import App from './App.vue';
+import { Default } from '@layout';
+import Vue from 'vue';
+import api from '@api';
 import i18n from './plugins/i18n';
 import router from '@router';
 import store from '@store';
@@ -12,6 +12,7 @@ import vuetify from './plugins/vuetify';
 Vue.component('default-layout', Default);
 
 Vue.config.productionTip = false;
+Vue.prototype.$api = api;
 
 new Vue({
   vuetify,
