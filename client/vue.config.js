99784d5a5cbaef80b235464d5c5e4b320d5137ba
Yaronberg <yaronmen123@gmail.com>
2020-07-17 18:28:11 +0300

    #3 add new aliases in config.js

--- client/vue.config.js
+++ client/vue.config.js
@@ -15,11 +15,10 @@ module.exports = {
       .set('@layout', path.resolve('src/layouts/'))
       .set('@locales', path.resolve('src/locales/'))
       .set('@constants', path.resolve('src/constants/'))
+      .set('@config', path.resolve('src/config/config.dev.json'))
+      .set('@api', path.resolve('src/services/api-service.service.js'))
   }
 };
 
 // .set('@assets', path.resolve('src/assets/'))
 // .set('@filters', path.resolve('src/filters/'))
-// .set('@config', path.resolve('src/config/config.dev.json'))
-// .set('@api', path.resolve('src/services/api-service.service.js'))
-
