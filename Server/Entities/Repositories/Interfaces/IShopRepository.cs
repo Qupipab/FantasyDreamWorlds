39c9570d387ef8777258e53a467c770d498fa1f7
Qupipab <qupipab@gmail.com>
2020-08-12 22:27:50 +0300

    #65 add ShopRepository

--- Server/Entities/Repositories/Interfaces/IShopRepository.cs
+++ Server/Entities/Repositories/Interfaces/IShopRepository.cs
@@ -1,10 +1,27 @@
-﻿using System;
+﻿using Entities.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Entities.Repositories.Interfaces
 {
   public interface IShopRepository
   {
+
+    Task<bool> CreateGameServerAsync(GameServer gameServer);
+    Task<bool> EditGameServerAsync(GameServer gameServer);
+    Task<bool> RemoveGameServerAsync(GameServer gameServer);
+
+
+    Task<bool> CreateCategoryAsync(Category category);
+    Task<bool> EditCategoryAsync(Category category);
+    Task<bool> RemoveCategoryAsync(Category category);
+
+
+    Task<bool> CreateItemAsync(Item item);
+    Task<bool> EditItemAsync(Item item);
+    Task<bool> RemoveItemAsync(Item item);
+
   }
 }
--- Server/Entities/Repositories/ShopRepository.cs
+++ Server/Entities/Repositories/ShopRepository.cs
@@ -1,11 +1,70 @@
-﻿using Entities.Repositories.Interfaces;
+﻿using Entities.Models;
+using Entities.Repositories.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Entities.Repositories
 {
   public class ShopRepository : IShopRepository
   {
+
+
+    // <---------- GameServer ---------->
+
+
+    public Task<bool> CreateGameServerAsync(GameServer gameServer)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task<bool> EditGameServerAsync(GameServer gameServer)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task<bool> RemoveGameServerAsync(GameServer gameServer)
+    {
+      throw new NotImplementedException();
+    }
+
+
+    // <---------- Category ---------->
+
+
+    public Task<bool> CreateCategoryAsync(Category category)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task<bool> EditCategoryAsync(Category category)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task<bool> RemoveCategoryAsync(Category category)
+    {
+      throw new NotImplementedException();
+    }
+
+
+    // <---------- Item ---------->
+
+
+    public Task<bool> CreateItemAsync(Item item)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task<bool> EditItemAsync(Item item)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task<bool> RemoveItemAsync(Item item)
+    {
+      throw new NotImplementedException();
+    }
   }
 }
